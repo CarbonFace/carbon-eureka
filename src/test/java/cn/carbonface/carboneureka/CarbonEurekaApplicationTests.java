90a0baaf6f2948f798c72e3daff64f6dd7defe5d
CarbonFace <553127022@qq.com>
2021-03-25 21:23:42 +0800

    1、carbon-eureka project init
    2021/3/25  Xiaoyi Ye

---
+++ src/test/java/cn/carbonface/carboneureka/CarbonEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.carbonface.carboneureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
